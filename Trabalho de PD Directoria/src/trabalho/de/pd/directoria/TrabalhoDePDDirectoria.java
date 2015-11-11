2a223332141cdbddd0388fe1589321733916bc4d
Rafael Francisco <rafaelfrancisco6@gmail.com>
2015-11-11 16:00:45 +0000

    Cenas

--- Trabalho de PD Directoria/src/trabalho/de/pd/directoria/TrabalhoDePDDirectoria.java
+++ Trabalho de PD Directoria/src/trabalho/de/pd/directoria/TrabalhoDePDDirectoria.java
@@ -5,17 +5,53 @@
  */
 package trabalho.de.pd.directoria;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
 /**
  *
  * @author Rafael
  */
 public class TrabalhoDePDDirectoria {
 
+    static final int port = 7000;
+    static String EndIP;
+    
     /**
      * @param args the command line arguments
+     * @throws java.net.SocketException
+     * @throws java.net.UnknownHostException
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws UnknownHostException, IOException{
+        receberIP();
+        enviarIP();
     }
     
+    public static void receberIP() throws SocketException, UnknownHostException, IOException{      
+        byte[] receiveData = new byte[1024];
+        
+        System.out.println("A iniciar servidor de directoria...");
+
+        DatagramSocket clientSocket = new DatagramSocket(port);
+
+        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+        clientSocket.receive(receivePacket);
+
+        EndIP = receivePacket.getAddress().toString();
+        
+        clientSocket.close();
+        
+        System.out.println("O IP do servidor primário é " + EndIP);
+    }
+    
+    public static void enviarIP(){
+        do{
+            
+        }while(true);                                         //Eliminar esta bosta
+    }
+
 }
