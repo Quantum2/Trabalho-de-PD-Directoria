03a9de598d42a98b12042e8f672078f063ed8c16
Rafael Francisco <rafaelfrancisco6@gmail.com>
2015-11-11 18:02:13 +0000

    Mais Signed-off-by:Rafael Francisco <rafaelfrancisco6@gmail.com>

--- Trabalho de PD Directoria/src/trabalho/de/pd/directoria/TrabalhoDePDDirectoria.java
+++ Trabalho de PD Directoria/src/trabalho/de/pd/directoria/TrabalhoDePDDirectoria.java
@@ -18,6 +18,7 @@ import java.net.UnknownHostException;
 public class TrabalhoDePDDirectoria {
 
     static final int port = 7000;
+    static final int timeToWait = 5000;
     static String EndIP;
     static boolean exec = true;
     
@@ -25,8 +26,9 @@ public class TrabalhoDePDDirectoria {
      * @param args the command line arguments
      * @throws java.net.SocketException
      * @throws java.net.UnknownHostException
+     * @throws java.lang.InterruptedException
      */
-    public static void main(String[] args) throws UnknownHostException, IOException{
+    public static void main(String[] args) throws UnknownHostException, IOException, SocketException, InterruptedException{
         receberIP();
         enviarIP();
     }
@@ -46,17 +48,22 @@ public class TrabalhoDePDDirectoria {
         System.out.println("O IP do servidor primário é " + EndIP);
     }
     
-    public static void enviarIP() throws SocketException, IOException{
+    public static void enviarIP() throws SocketException, IOException, InterruptedException{
         do{
             byte[] receiveData = new byte[1024];
+            String temp;
             
             System.out.println("A procura de clientes...");
             
             DatagramSocket clientSocket = new DatagramSocket(port);
             DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-            clientSocket.receive(receivePacket);     
+            clientSocket.receive(receivePacket);    
             
+            System.out.println("Encontrado um cliente...");
             
+            temp = new String(receivePacket.getData());
+            
+            Thread.sleep(timeToWait);
         }while(exec);                                        
     }
 
